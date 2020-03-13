3d1c1323a8c428f15950733cf6356c471df84a67
Arjun-Siva <arjunfromtnj@gmail.com>
2020-03-13 20:36:35 +0530

    Updated to rails resources convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get "todos", to: "todos#index"
-  get "todos/create", to: "todos#create"
-  get "todos/:id", to: "todos#show"
-  get "todos/create", to: "todos#create"
+resources :todos
 end
