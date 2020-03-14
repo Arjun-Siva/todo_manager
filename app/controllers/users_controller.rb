221a558a71b99829d1d6aa512395cecb38526d37
Arjun-Siva <arjunfromtnj@gmail.com>
2020-03-14 20:33:59 +0530

    Formatted with rufo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,32 @@
 # users_controller.rb
 class UsersController < ApplicationController
-    skip_before_action :verify_authenticity_token
-      def index
-        render plain: User.all.map{|user| user.to_pleasant_string}.join("\n")
-      end
-  
-    def create
-      name=params[:name]
-      email=params[:email]
-      password=params[:password]
-      if(email.include? "@")
-      new_user=User.create!(
-        name: name,
-        email: email,
-        password: password,
-      )
-      response_text="New user is created with the id #{new_user.id}"
-      render plain:response_text
-      else
-      render plain:"Invalid email format"
-      end
-    end
+  skip_before_action :verify_authenticity_token
 
-    def login
-        email=params[:email]
-        password=params[:password]
-        user_record= User.find_by email: email
-        if(user_record!=nil && user_record.password.to_s==password)
-            render plain:"true"
-        else
-            render plain:"false"
-        end
+  def index
+    render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+    response_text = "New user is created with the id #{new_user.id}"
+    render plain: response_text
+  end
+
+  def login
+    email = params[:email]
+    password = params[:password]
+    user_record = User.find_by email: email
+    if (user_record != nil && user_record.password.to_s == password)
+      render plain: "true"
+    else
+      render plain: "false"
     end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-resources :todos
-resources :users
-post "users/login", to: "users#login"
+  resources :todos
+  resources :users
+  post "users/login", to: "users#login"
 end
