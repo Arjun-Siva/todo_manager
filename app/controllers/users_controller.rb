ef88fdedad829737e224f4e67e6e54bef4812bb7
Arjun-Siva <arjunfromtnj@gmail.com>
2020-03-14 20:14:02 +0530

    Added users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+# users_controller.rb
+class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
+      def index
+        render plain: User.all.map{|user| user.to_pleasant_string}.join("\n")
+      end
+  
+    def create
+      name=params[:name]
+      email=params[:email]
+      password=params[:password]
+      if(email.include? "@")
+      new_user=User.create!(
+        name: name,
+        email: email,
+        password: password,
+      )
+      response_text="New user is created with the id #{new_user.id}"
+      render plain:response_text
+      else
+      render plain:"Invalid email format"
+      end
+    end
+
+    def login
+        email=params[:email]
+        password=params[:password]
+        user_record= User.find_by email: email
+        if(user_record!=nil && user_record.password.to_s==password)
+            render plain:"true"
+        else
+            render plain:"false"
+        end
+    end
+end
