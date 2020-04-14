d491e64d8c540cc06959d32fb669a55106cf0e71
Arjun-Siva <arjunfromtnj@gmail.com>
2020-04-14 12:50:48 +0530

    Added signup forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,15 @@
-# users_controller.rb
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
-  def index
-    render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
+  def new
+    render "users/new"
   end
 
   def create
-    name = params[:name]
-    email = params[:email]
-    password = params[:password]
-    new_user = User.create!(
-      name: name,
-      email: email,
-      password: password,
+    User.create!(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
     )
-    response_text = "New user is created with the id #{new_user.id}"
-    render plain: response_text
-  end
-
-  def login
-    email = params[:email]
-    password = params[:password]
-    user_record = User.find_by email: email
-    if (user_record != nil && user_record.password.to_s == password)
-      render plain: "true"
-    else
-      render plain: "false"
-    end
+    redirect_to "/"
   end
 end
